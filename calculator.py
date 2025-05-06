dc61de5469e783cba688ec4bf32d379801b6cd84
Jc Canlas <johnrolandchristopher@gmail.com>
2025-05-07 02:09:46 +0800

    Initial Commit
    This is my first ever commit in Git.

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# Adding Two Integer Numbers
+
+firstNum = int(input("Enter firstNumber: "))
+secondNum = int(input("Enter secondNumber: "))
+sumOfTwoNum = firstNum + secondNum
+
+print("The sum of two number is: ", sumOfTwoNum)
