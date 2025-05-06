15ea38538d7b4fb88cdc4098de717f8adfa59c00
Jc Canlas <johnrolandchristopher@gmail.com>
2025-05-07 02:56:55 +0800

    Committed the rest of file

--- calculator.py
+++ calculator.py
@@ -4,7 +4,11 @@ firstNum = int(input("Enter firstNumber: "))
 secondNum = int(input("Enter secondNumber: "))
 sumOfTwoNum = firstNum + secondNum
 diffOfTwoNum = firstNum - secondNum
+productOfTwoNum = firstNum * secondNum
+qoutOfTwoNum = firstNum / secondNum
 
 
 print("The sum of two number is: ", sumOfTwoNum)
 print("The difference of two number is: ", diffOfTwoNum)
+print("The difference of two number is: ", productOfTwoNum)
+print("The difference of two number is: ", qoutOfTwoNum)
